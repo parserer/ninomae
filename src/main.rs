3c0cc8a3ab6e53f50366dc4d1a0dc0a3e156ca7b
Reza <rezahandzalah@gmail.com>
2024-01-04 04:50:48 +0700

    test ci push
    tests whether dirty commit fails ci

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+/// For example, the human friendly byte strings
+///
+/// "0C 04 4A6F686E" encodes
+/// UTF8String, length 4, "John"; and
+///
+/// "02 01 19" encodes
+/// INTEGER, length 1, 25.
+fn decode_human(s: &str) -> Result<Vec<u8>, std::num::ParseIntError> {
+    let clean: String = s.chars().filter(|x| !x.is_whitespace()).collect();
+
+    let n = 2;
+    (0..(clean.len()))
+        .step_by(n)
+        .map(|i| u8::from_str_radix(&clean[i..(i + n)], 16))
+        .collect()
 }
 
-#[test]
-fn test_1() {
-    assert!(true);
+fn parse(tlv: &[u8]) -> Option<i64> {
+    let l = tlv.get(1).expect("wrong input length");
+    println!("expect get len ok: {:?}", l);
+    // ?? expect int or string tag, valuate return generic
+    // let tag_byte = tlv.get(0).expect("empty input");
+    // match tag_byte {
+    //     b"02" => Some()
+    //     _ => 
+    // }
+    match l {
+        _ => Some(0)
+    }
+}
+
+/// 255 -> '01 FF'
+fn as_tlv_bytes(num: u8) -> Vec<u8> {
+    vec![]
+}
+
+fn main() {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_1() {
+        match parse(&decode_human("0C 04 4A6F686E").unwrap()) {
+            None => assert!(false),
+            _ => {}
+        }
+    }
 }
