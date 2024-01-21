1da37f50f0821dda5c9640b5b30fad0dc5d1994b
razzzp <m.razif.pramuda@gmail.com>
2024-01-21 19:08:13 +0700

    tlv length checking

--- src/tlv_parser/tlv.rs
+++ src/tlv_parser/tlv.rs
@@ -1,4 +1,4 @@
-use num::{BigUint, Zero};
+use num::{BigUint, Zero, traits::ToBytes, BigInt};
 
 
 #[derive(Debug, Clone)]
@@ -12,10 +12,23 @@ impl EncodingData{
         EncodingData { identifier, length: None, content: None}
     }
     pub fn is_length_limit_reached(&self) -> bool{
-        todo!()
+        if let Some(length) = self.length.as_ref() {
+            if length.length.is_zero() {return true;}
+
+            if let Some(content) = self.content.as_ref(){
+                if length.length <= content.bytes_length(){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
-    pub fn bits(&self) -> u64{
-        todo!()
+    pub fn bytes_length(&self) -> u32{
+        return self.identifier.bytes_length()
+            + self.length.as_ref().map_or(0, |l| l.bytes_length())
+            + self.content.as_ref().map_or(0, |c| c.bytes_length())
     }
 }
 
@@ -26,6 +39,14 @@ pub struct Identifier{
     pub tag_number: u32
 }
 impl Identifier  {
+    pub fn bytes_length(&self) -> u32{
+        if self.tag_number < 31{
+            return 1;
+        } else {
+            // enough if max identifier length is 4 bytes
+            return self.tag_number.to_ne_bytes().len() as u32;
+        }
+    }
 }
 
 #[derive(Debug,Clone,PartialEq)]
@@ -47,6 +68,9 @@ pub struct Length{
     pub length: u32
 }
 impl Length{
+    pub fn bytes_length(&self) -> u32{
+        return self.length.to_ne_bytes().len() as u32;
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -57,15 +81,32 @@ pub enum Content{
     Raw(Vec<u8>)
 }
 impl Content {
+    pub fn bytes_length(&self) -> u32{
+        let len = match self {
+            Content::Primitive(pc) => pc.bytes_length(),
+            Content::Constructed(children) => children.iter().fold(0, |acc, data| acc + data.bytes_length()),
+            Content::Raw(r) => r.len() as u32
+        };
+        return len
+    }
 }
 
 
 #[derive(Debug, Clone)]
 pub enum PrimitiveContent{
     Boolean(bool),
-    Integer(BigUint),
+    Integer(BigInt),
     OctetString(Vec<u8>),
     UTF8String(String)
 }
 impl PrimitiveContent{
+    pub fn bytes_length(&self) -> u32{
+        let len = match self {
+            PrimitiveContent::Boolean(_) => 1,
+            PrimitiveContent::Integer(i) => (i.bits()/8) as u32,
+            PrimitiveContent::OctetString(os) => os.len() as u32,
+            PrimitiveContent::UTF8String(ut8_str) => ut8_str.len() as u32,
+        };
+        return len
+    }
 }
